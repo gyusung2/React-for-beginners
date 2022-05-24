4436ca8666d7cce0eaf47ed09d3e319895bab318
gyusung117 <97gyusung@gmail.com>
2022-05-24 10:12:07 +0900

    fix : jsx 문법 3개

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
+// css 경로
 import './App.css';
 
 function App() {
+
+    let post = '대전 선화동 맛집';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* // className으로 작성 할 것  */}
+    <div className="black-nav">
+      <h4 style={{color : 'royalblue' , fontSize :'30px'}}>블로그</h4>
+    </div>
+    <h4>{post}</h4>
     </div>
   );
 }
