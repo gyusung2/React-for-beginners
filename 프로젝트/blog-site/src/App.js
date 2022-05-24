11df599475f6b8a047003e73441f0edc86fb0dbf
gyusung117 <97gyusung@gmail.com>
2022-05-24 15:32:36 +0900

    fix : array , object ,state 관련 내용

--- 프로젝트/blog-site/src/App.js
+++ 프로젝트/blog-site/src/App.js
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -5,28 +6,42 @@ import { useState } from 'react';
 function App() {
   let post = '대전 맛집'
   let [logo , setLogo] = useState('음식Blog');
-  let [글제목, b] = useState(['탄방동 음식 추천','선화동 음식 추천','월평동 음식 추천']);
+  let [글제목, 글제목변경] = useState(['탄방동 음식 추천','선화동 음식 추천','월평동 음식 추천']);
+  let [따봉 , 따봉변경] = useState(0);
+  //          state 변경 함수
   return (
     <div className="App">
      <div className='black-nav'>
        <h4>{logo}</h4>
      </div>
      <div className='list'>
-       <h4>{글제목[0]}</h4>
+       <h4>{글제목[0]}<span onClick={()=>{ 따봉변경(따봉+1)}}>👍</span>{따봉}</h4>
        <p>5월 24일 발행</p>
 
      </div>
      <div className='list'>
-       <h4>{글제목[1]}</h4>
+       <h4>{글제목[1]}<span>👍</span></h4>
        <p>5월 24일 발행</p>
 
      </div>
      <div className='list'>
-       <h4>{글제목[2]}</h4>
+       <h4>{글제목[2]}<span>👍</span></h4>
        <p>5월 24일 발행</p>
 
      </div>
-    
+      <button onClick={()=>{
+        let copy =[...글제목];
+        copy.sort();
+        글제목변경(copy)
+      }}>가나다라순정렬</button>
+
+
+      <button onClick={ ()=>{ 
+        let copy = [...글제목];
+        copy[0] = '미세노 센세';
+        글제목변경(copy)
+      } }> 수정버튼 </button>
+      {/* <button onClick={()=>{ b(['미세노 센세','선화동 음식 추천','월평동 음식 추천'])}}>Click !</button> */}
     </div>
   );
 }
