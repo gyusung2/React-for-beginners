f8b4c147599328046f51ed7f3fcb19e01218e6e7
gyusung117 <97gyusung@gmail.com>
2022-05-25 12:41:30 +0900

    feat : export , import 내용

---
+++ 프로젝트/export,import/home.js
@@ -0,0 +1,29 @@
+// function getName() {
+//     return 'hello';
+// }
+
+
+// let var 데이터 타입
+// var 중복 가능
+// let 중복 불가능
+// const 수정 불가능 할 때
+// const PI = 3.14;
+
+
+// //1. 
+// function getName(){
+//     return 'hello';
+// }
+
+
+//2.
+export const getName = function() {
+    return 'hello';
+}
+// 
+
+
+export const getAge = () => {
+    return 10;
+}
+
---
+++ 프로젝트/export,import/index.js
@@ -0,0 +1,8 @@
+// export 보내고 [home.js] > import 받는 거[index.js]
+
+import { getName , getAge } from "./home.js";
+
+console.log(getName());
+console.log(getAge());
+
+
