e742780bcd45db5e894801629d5f1768ba24fcd9
gyusung117 <97gyusung@gmail.com>
2022-06-03 11:16:58 +0900

    feat : 리액트 라우터 3

--- 프로젝트/기초 연습/shop/src/App.js
+++ 프로젝트/기초 연습/shop/src/App.js
@@ -49,7 +49,8 @@ function App(){
         </>
         }></Route>
 
-        <Route path="/detail" element= {<Detail></Detail>}></Route>
+        <Route path="/detail/:id" element= {<Detail shoes={shoes} ></Detail>}></Route>
+       
         
         <Route path='*' element={<div>없는 페이지 입니다.</div>}></Route>
 
--- 프로젝트/기초 연습/shop/src/routes/Detail.js
+++ 프로젝트/기초 연습/shop/src/routes/Detail.js
@@ -1,4 +1,14 @@
-function Detail(){
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail(props){
+
+    let {id} = useParams();
+    console.log(id);
+    let 찾은상품 = props.shoes.find(function(x){
+      return x.id == id
+    });
+  
     return(
       <div className="container">
       <div className="row">
@@ -6,9 +16,9 @@ function Detail(){
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h4 className="pt-5">{props.shoes[id].title}</h4>
+          <p>{props.shoes[id].content}</p>
+          <p>{props.shoes[id].price}원</p>
           <button className="btn btn-danger">주문하기</button> 
         </div>
       </div>
