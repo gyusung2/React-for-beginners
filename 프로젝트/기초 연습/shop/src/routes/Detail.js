d76a2352a9f0eef4d404c683dbd99ec797a38097
gyusung117 <97gyusung@gmail.com>
2022-06-03 11:23:12 +0900

    feat : 라우터 3

--- 프로젝트/기초 연습/shop/src/routes/Detail.js
+++ 프로젝트/기초 연습/shop/src/routes/Detail.js
@@ -16,9 +16,9 @@ function Detail(props){
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}원</p>
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
           <button className="btn btn-danger">주문하기</button> 
         </div>
       </div>
