595cd7328f5e0daf9cdcbe03bc4e0338978853aa
Gyusung Lee <97gyusung@gmail.com>
2022-06-07 00:43:03 +0900

    fix : 1 , 2 단계 숙제 완료

--- 프로젝트/shopshop/src/App.js
+++ 프로젝트/shopshop/src/App.js
@@ -30,33 +30,24 @@ function App() {
     <div className='main-bg'></div>
     <div className="container">
       <div className="row">
-        <div className="col-md-4">
-          <img src='https://codingapple1.github.io/shop/shoes1.jpg' width={"80%"}></img>
-          <h4>{shoes[0].title}</h4>
-          <p>{ shoes[0].price }</p>
-          </div>
-          <div className="col-md-4">
-          <img src='https://codingapple1.github.io/shop/shoes2.jpg' width={"80%"}></img>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-          </div>
-          <div className="col-md-4">
-          <img src='https://codingapple1.github.io/shop/shoes3.jpg' width={"80%"}></img>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-          </div>
+
+        <Card shoes ={shoes[0]} i={1}></Card>
+        <Card shoes ={shoes[1]} i={2}></Card>
+        <Card shoes ={shoes[2]} i={3}></Card>
+
+     
       </div>
 </div> 
   </div>
   );
 
 
-  function Card() {
+  function Card(props) {
     return(
       <div className="col-md-4">
-      <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-      <h4>{}</h4>
-      <p>상품정보</p>
+      <img src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
     </div>
     )
   
