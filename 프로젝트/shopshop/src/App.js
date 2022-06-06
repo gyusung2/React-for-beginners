35de35ba41ce3043e08654e0587dcabd6b62de14
Gyusung Lee <97gyusung@gmail.com>
2022-06-06 23:56:05 +0900

    fix : 2.2강의

--- 프로젝트/shopshop/src/App.js
+++ 프로젝트/shopshop/src/App.js
@@ -16,6 +16,27 @@ function App() {
       </Nav>
       </Container>
     </Navbar>
+
+    <div className='main-bg'></div>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <img src='https://codingapple1.github.io/shop/shoes1.jpg' width={"80%"}></img>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+          <img src='https://codingapple1.github.io/shop/shoes2.jpg' width={"80%"}></img>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+          <img src='https://codingapple1.github.io/shop/shoes3.jpg' width={"80%"}></img>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+          </div>
+      </div>
+</div> 
   </div>
   );
 }
