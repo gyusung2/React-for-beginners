3733b8fa310cbfc973b356af016d51dd52bc51e2
Gyusung Lee <97gyusung@gmail.com>
2022-06-07 00:26:17 +0900

    fix : import , export

--- 프로젝트/shopshop/src/App.js
+++ 프로젝트/shopshop/src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import {Button, Navbar, Container, Nav} from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js';
+
 // https://www.fwantastic.com/2020/03/react-reactstrap.html
 
+
+
 function App() {
+  
+
+  let [shoes] = useState(data)
+  
+
   return (
     <div>
     <Navbar bg="dark" variant="dark">
@@ -22,8 +32,8 @@ function App() {
       <div className="row">
         <div className="col-md-4">
           <img src='https://codingapple1.github.io/shop/shoes1.jpg' width={"80%"}></img>
-          <h4>상품명</h4>
-          <p>상품설명</p>
+          <h4>{shoes[0].title}</h4>
+          <p>{ shoes[0].price }</p>
           </div>
           <div className="col-md-4">
           <img src='https://codingapple1.github.io/shop/shoes2.jpg' width={"80%"}></img>
@@ -39,6 +49,19 @@ function App() {
 </div> 
   </div>
   );
+
+
+  function Card() {
+    return(
+      <div className="col-md-4">
+      <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+      <h4>{}</h4>
+      <p>상품정보</p>
+    </div>
+    )
+  
+  }
 }
+
 
 export default App;
