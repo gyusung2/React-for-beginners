a90eddcd91f199efde17b1846ad738db8b2841cd
Gyusung Lee <97gyusung@gmail.com>
2022-06-07 00:51:55 +0900

    fix : 3번째 숙제 완료

--- 프로젝트/shopshop/src/App.js
+++ 프로젝트/shopshop/src/App.js
@@ -31,9 +31,13 @@ function App() {
     <div className="container">
       <div className="row">
 
-        <Card shoes ={shoes[0]} i={1}></Card>
-        <Card shoes ={shoes[1]} i={2}></Card>
-        <Card shoes ={shoes[2]} i={3}></Card>
+        {
+            shoes.map((a, i)=>{
+              return (
+                <Card shoes={shoes[i]} i={i+1}></Card>
+              )
+            }) 
+        }
 
      
       </div>
@@ -45,7 +49,7 @@ function App() {
   function Card(props) {
     return(
       <div className="col-md-4">
-      <img src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"} width="80%" />
+      <img src={"https://codingapple1.github.io/shop/shoes" + (props.i+1) + ".jpg"} width="80%" />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.price}</p>
     </div>
