055fd0220ba73f107917be5346c72e03ad84a843
Gyusung Lee <97gyusung@gmail.com>
2022-06-08 00:56:00 +0900

    feat : 컴포넌트

--- 프로젝트/기초 연습/blog2/src/App.js
+++ 프로젝트/기초 연습/blog2/src/App.js
@@ -5,41 +5,52 @@ import { useState } from 'react';
 
 function App() {
   let post = ['나이키'];
-  let [글제목 , 글제목변경] = useState(['스포츠 브랜드 추천', '의류 브랜드 추천', '신발 브랜드 추천'])
-  let [like , setLike] = useState(0)
+  let [title , setTitle] = useState(['스포츠 브랜드 추천', '의류 브랜드 추천', '신발 브랜드 추천'])
+  let [like , setLike] = useState(0);
+  let [modal ,setModal] = useState(false);
   return (
     
     <div className="App">
      <div className='black-nav'>
-       <h4>블로그</h4>
+       <h4>미니블로그</h4>
      </div>
      <div className="list">
-        <h4>{글제목[0]}<span onClick={()=>{
+        <h4>{title[0]}<span onClick={()=>{
           setLike(like + 1)
         }}>👍</span>{like}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[1]}</h4>
+        <h4>{title[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4 onClick={()=>{
+          setModal(!modal)
+        }}>{title[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
-      <button onClick={()=>{
-          let copy = [...글제목];
-          copy[0] = '나이키';
-          글제목변경(copy)
-        }}>클릭 !!</button>
-        
-         <button onClick={()=>{
-          let copy = [...글제목];
-          copy.sort();
-          글제목변경(copy)
-        }}>정렬 클릭 !!</button>
+    
+  {
+  modal == true ? <Modal></Modal> : null
+
+  }
+         
     </div>
+    
   );
 }
 
+function Modal() {
+  return (
+    <>    
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+    </>
+
+  )
+}
 export default App;
