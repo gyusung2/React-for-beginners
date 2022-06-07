aed06dfc3019b247c1a77cf4663f2cc091a93c5f
Gyusung Lee <97gyusung@gmail.com>
2022-06-08 02:10:33 +0900

    fix : props 해결 하기

--- 프로젝트/기초 연습/blog2/src/App.js
+++ 프로젝트/기초 연습/blog2/src/App.js
@@ -5,33 +5,52 @@ import { useState } from 'react';
 
 function App() {
   let post = ['나이키'];
-  let [title , setTitle] = useState(['스포츠 브랜드 추천', '의류 브랜드 추천', '신발 브랜드 추천'])
-  let [like , setLike] = useState([0,0,0]);
-  let [modal ,setModal] = useState(false);
+  let [title, setTitle] = useState(['스포츠 브랜드 추천', '의류 브랜드 추천', '신발 브랜드 추천'])
+  let [like, setLike] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false);
+  let [입력값, 입력값변경] = useState('');
+  let [sidetitle , setSideTitle] = useState(0);
   return (
-    
+
     <div className="App">
-     <div className='black-nav'>
-       <h4>미니블로그</h4>
-       </div>
-       {
-        title.map(function(a , i){
-          return(
+      <div className='black-nav'>
+        <h4>미니블로그</h4>
+      </div>
+
+
+      {
+        title.map(function (a, i) {
+          return (
             <div className="list">
-            <h4 onClick={()=>{
-            setModal(!modal)
-            }}>{title[i]}<span onClick={()=>{
-            let copy = [...like];
-            copy[i] = copy[i] + 1;
-            setLike(copy)
-           }}>👍</span>{like[i]}</h4>
-          <p>2월 17일 발행</p>
-          </div>
+              <h4 onClick={() => {
+                setModal(!modal); setSideTitle(i)
+              }}>{title[i]}
+                <span onClick={() => {
+                  let copy = [...like];
+                  copy[i] = copy[i] + 1;
+                  setLike(copy)
+                }}>👍</span>{like[i]}</h4>
+              <p>2월 17일 발행</p>
+              <button onClick={()=>{ 
+              let copy = [...title];
+              copy.splice(i, 1);
+              setTitle(copy);
+           }}>삭제</button>
+            </div>
           )
         })
-       }
-    
-     {/* <div className="list">
+      }
+
+<div>
+      <input onChange={ (e)=>{ 입력값변경(e.target.value) } } />
+      <button onClick={()=>{ 
+        let copy = [...title];
+        copy.unshift(입력값);
+        setTitle(copy)
+      }}>글발행</button>
+    </div>
+
+      {/* <div className="list">
         <h4 onClick={()=>{
            setModal(!modal)
         }}>{title[0]}<span onClick={()=>{
@@ -48,25 +67,32 @@ function App() {
         <p>2월 17일 발행</p>
       </div> */}
 
-         
-  {
-  modal == true ? <Modal></Modal> : null
 
-  }
-         
+      {
+        modal == true ? <Modal title ={title} sidetitle={sidetitle} ></Modal> : null
+
+      }
+
     </div>
-    
+
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
-    <>    
-    <div className='modal'>
-      <h4>제목</h4>
-      <p>날짜</p>
-      <p>상세내용</p>
-    </div>
+    <>
+      <div className='modal'>
+        <h4>{ props.title[props.sidetitle] }</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      <button onClick={()=>{ //해결 못함
+      props.setTitle(); 
+       let copy =[...title];
+       copy.props
+       copy[0] = '슈퍼스타';
+       setTitle(copy)}}>글 수정</button>
+      </div>
+      
     </>
 
   )
