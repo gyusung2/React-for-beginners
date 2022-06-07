787dd34b0ce790d2b23388062dfaf2cb98231954
gyusung117 <97gyusung@gmail.com>
2022-06-07 15:05:18 +0900

    fix :Lifecycle과 useEffect 2

--- 프로젝트/기초 연습/shop/src/routes/Detail.js
+++ 프로젝트/기초 연습/shop/src/routes/Detail.js
@@ -2,17 +2,23 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from 'styled-components';
 
-let YellowBtn = styled.button`
-background: ${props => props.bg};
-color : black;
-padding : 10px;
-`
-let Box = styled.div `
-background : grey;
-padding : 20px;
-`
+// let YellowBtn = styled.button`
+// background: ${props => props.bg};
+// color : black;
+// padding : 10px;
+// `
+// let Box = styled.div `
+// background : grey;
+// padding : 20px;
+// `
 
 function Detail(props){
+    useEffect(()=>{
+      setTimeout(()=>{ setAlert(false) }, 2000)
+    }, [])
+    let [alert , setAlert] = useState(true)
+    let [count , setCount] = useState(0)
+
     let {id} = useParams();
     let 찾은상품 = props.shoes.find(function(x){
       return x.id == id
@@ -20,10 +26,21 @@ function Detail(props){
     
   return(
         <div className="container">
-          <Box>
+          {
+            alert == true ?
+          <div className="alert alert-warning">
+            2초이내 구매하세요 !
+          </div>
+          :null
+
+          }
+          {count}
+          <button onClick={()=>{setCount(count+1)
+          }}>버튼</button>
+          {/* <Box>
           <YellowBtn bg="blue">버튼</YellowBtn>
           <YellowBtn bg="royalblue">버튼</YellowBtn>
-          </Box>
+          </Box> */}
           <div className="row">
             <div className="col-md-6">
               <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
