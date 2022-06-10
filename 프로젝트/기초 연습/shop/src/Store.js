63be3e6219c92c96b5947684a16f05c9be053aab
gyusung117 <97gyusung@gmail.com>
2022-06-10 16:28:33 +0900

    fix : Redux 3 : store의 state 변경하는 법

--- 프로젝트/기초 연습/shop/src/Store.js
+++ 프로젝트/기초 연습/shop/src/Store.js
@@ -1,30 +1,44 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
-import user from './store/userSlice.js'
 
 
 
 
 
-let stock = createSlice({
+
+let user = createSlice({
+    name : 'user',
+    initialState : 'kim',
+    reducers : {
+        nameChange(state){
+        return 'john kim'
+
+        }
+    }
+  })
+
+  export let {nameChange} = user.actions
+
+  let stock = createSlice({
     name : 'stock',
     initialState : [10, 11, 12]
-})
+  })
 
-let cart = createSlice({
-    name: 'cart',
-   initialState :  [
+  let item = createSlice({
+      name : 'item',
+      initialState :[
         {id : 0, name : 'White and Black', count : 2},
         {id : 2, name : 'Grey Yordan', count : 1}
       ] 
-})
+  })
+
+
 
 
 export default configureStore({
-  reducer: {
+    reducer: {
       user : user.reducer,
-      stock  :stock.reducer,
-      cart : cart.reducer,
-      increase: increase.reducer
-   }
-}) 
+      stock : stock.reducer,
+      item : item.reducer
+    }
+  }) 
 
--- 프로젝트/기초 연습/shop/src/routes/Cart.js
+++ 프로젝트/기초 연습/shop/src/routes/Cart.js
@@ -1,41 +1,45 @@
 import { Table } from 'react-bootstrap' ;
-import { useDispatch, useSelector } from 'react-redux';
-import { changeName } from '../Store';
+import {useDispatch, useSelector } from 'react-redux';
+import {nameChange} from "./../Store.js"
+
 
 
 
 function Cart() {
 
-   let state = useSelector((state)=>{return state})
-   console.log(state.stock);
-   let dispatch =  useDispatch()
+    let state = useSelector((state) => { return state } )
+    let dispatch = useDispatch();
+
     return (
-        <div>
-            {state.user.name}의 장바구니
-                    <Table>
-            <thead>
-                <tr>
-                <th>#</th>
-                <th>상품</th>
-                <th>수량</th>
-                <th>변경하기</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                state.cart.map((a, i)=>
-                <tr key={i}>
-                <td>{1}</td>
-                <td>{state.cart[i].name}</td>
-                <td>{state.cart[i].count}</td>
-                <td><button onClick={()=>{
-                    dispatch(changeName())
-                }}>+</button></td>
-                </tr>
-                 )
-                }
-            </tbody>
-            </Table> 
+        <div> 
+
+            {state.user}의 장바구니
+       <Table>
+        <thead>
+
+            <tr>
+            <th>#</th>
+            <th>상품명</th>
+            <th>수량</th>
+            <th>변경하기</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+            state.item.map((a,i)=>
+            <tr key={i}>
+            <td>1</td>
+            <td>{state.item[i].name}</td>
+            <td>{state.item[i].count}</td>
+            <td>안녕</td>
+            <td><button onClick={()=>{
+                 dispatch(nameChange())
+            }}>클릭</button></td>
+            </tr>
+            )   
+            }
+        </tbody>
+        </Table> 
         </div>
     )
 }
