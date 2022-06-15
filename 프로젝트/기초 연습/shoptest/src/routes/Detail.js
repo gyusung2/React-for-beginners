e8a1cb20045e97c50089414458855c1495d944e1
Gyusung Lee <97gyusung@gmail.com>
2022-06-15 23:29:31 +0900

    fix : 리액트 라우터 3 : URL 파라미터로 상세페이지 100개 만들기[find 다시보기 ]

--- 프로젝트/기초 연습/shoptest/src/routes/Detail.js
+++ 프로젝트/기초 연습/shoptest/src/routes/Detail.js
@@ -2,23 +2,25 @@ import { useParams } from "react-router-dom";
 
 function Detail(props) {
 
-let {id} =  useParams();
-
+  let { id } = useParams();
+  let 찾은상품 = props.shoes.find(function (x) {
+    return x.id == id
+  });
 
   return (
     <div className="container">
-  <div className="row">
-    <div className="col-md-6">
-      <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+      <div className="row">
+        <div className="col-md-6">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
     </div>
-    <div className="col-md-6">
-      <h4 className="pt-5">{props.shoes[id].title}</h4>
-      <p>{props.shoes[id].content}</p>
-      <p>{props.shoes[id].price}</p>
-      <button className="btn btn-danger">주문하기</button> 
-    </div>
-  </div>
-</div> 
   )
 }
 
