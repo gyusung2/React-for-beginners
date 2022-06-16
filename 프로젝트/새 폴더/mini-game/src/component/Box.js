c1a4679ebee64690749099ed88bb121004a971f2
Gyusung Lee <97gyusung@gmail.com>
2022-06-17 02:24:57 +0900

    feat : 가위바위보 게임 구현

--- 프로젝트/새 폴더/mini-game/src/component/Box.js
+++ 프로젝트/새 폴더/mini-game/src/component/Box.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+// rafce 컴포넌트 단축키
 const Box = (props) => {
-
+  console.log("props", props);
   return (
     <div className='box'>
- 
- 
+      <h1>{props.title}</h1>
+      <img className='item-img' src={props.item && props.item.img}></img>
+      <h2>{props.result}</h2>
+    
     </div>
   )
 }
