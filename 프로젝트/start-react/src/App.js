a892861e43aaa9db652c2e0dee1db5a3a7438ed9
2gyusung <97gyusung@gmail.com>
2022-09-20 23:59:55 +0900

    fix : 수정

--- 프로젝트/start-react/src/App.js
+++ 프로젝트/start-react/src/App.js
@@ -1,65 +1,84 @@
 import logo from './logo.svg';
 import './App.css'; //경로
-import {useState} from 'react';
+import { useState } from 'react';
 
 function App() {
   let post = '강남 우동 맛집';
-  let [글제목 , 글제목변경] = useState(['남자 코트 추천', '맛집추천', '파이선독학'])
-  let [따봉, 따봉변경] = useState(0);
-  let [modal , setModal] = useState(false);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '맛집추천', '파이선독학'])
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false);
+
 
 
   return (
     <div className="App">
       <div>
-        <div className ='black-nav'>
-        <h4>React </h4>
+        <div className='black-nav'>
+          <h4>React </h4>
         </div>
         <div>
-          <button onClick={()=>{
+          <button onClick={() => {
             let copy = [...글제목];
             copy.sort();
-            글제목변경(copy)}}>수정</button>
-          <div className = 'list'>
+            글제목변경(copy)
+          }}>수정</button>
+
+          {/* <div className='list'>
             <h4>
-              {글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉}
+              {글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span> {따봉}
             </h4>
             <p>2월 17일 발행</p>
           </div>
-          <div className = 'list'>
+          <div className='list'>
             <h4>
               {글제목[1]}
             </h4>
             <p>2월 17일 발행</p>
           </div>
-          <div className = 'list'>
+          <div className='list'>
             <h4>
               {글제목[2]}
             </h4>
             <p>2월 17일 발행</p>
-          </div>
+          </div> */}
 
-            <button  onClick={()=>{setModal(!modal)}}>모달</button>
-            {
-              modal == true ? <Modal/> : null
-            }
 
-          {/* <Modal></Modal> */}
+          {
 
-        
+            글제목.map(function (a, i) {
+              return <div className='list'>
+                <h4>
+                  {글제목[i]} <span onClick={()=> { 
+                    let copy = [...따봉];
+                  copy[i] = copy[i] + 1;
+                  따봉변경(copy)}}>👍</span> {따봉[i]}
+                </h4>
+                <p>2월 17일 발행</p>
+              </div>
+            })
+          }
 
-        </div>
+        <button onClick={() => { setModal(!modal) }}>모달</button>
+        {
+          modal == true ? <Modal /> : null
+        }
+
+        {/* <Modal></Modal> */}
+
+
+
       </div>
     </div>
+    </div >
   );
 }
-function Modal(){ //컴포넌트
-  return(
+function Modal() { //컴포넌트
+  return (
     <div className='modal'>
-    <h4>제목</h4>
-    <p>날짜</p>
-    <p>상세내용</p>
-  </div>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
   )
 }
 
