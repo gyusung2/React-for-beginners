5e26896743714c815e4b34fbb562ccd6edece7a4
2gyusung <97gyusung@gmail.com>
2022-11-12 17:09:05 +0900

    feat: jsx 문법

--- 프로젝트/연습/remind/src/App.js
+++ 프로젝트/연습/remind/src/App.js
@@ -2,22 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = '강남 우동 맛집'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{color: 'blue'}} className='black-nav'>
+        <h4>블로그</h4>
+      </div>
+        {post}
     </div>
   );
 }
