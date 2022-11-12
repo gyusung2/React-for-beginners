a917435d3240e5bf2f63c72cf729a4f58d734e39
2gyusung <97gyusung@gmail.com>
2022-11-12 17:38:50 +0900

    fix : 수정

--- 프로젝트/연습/remind/src/App.js
+++ 프로젝트/연습/remind/src/App.js
@@ -1,15 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집'
+  let [글제목,b] = useState(['남자 코트 추천','강남','부산']);
   return (
     <div className="App">
-      <div style={{color: 'blue'}} className='black-nav'>
+      <div style={{ color: 'blue' }} className='black-nav'>
         <h4>블로그</h4>
       </div>
-        {post}
+      <div className='list'>
+        <h4>{글제목[0]}</h4>
+        <p> 11월 12일</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p> 11월 12일</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p> 11월 12일</p>
+      </div>
     </div>
   );
 }
