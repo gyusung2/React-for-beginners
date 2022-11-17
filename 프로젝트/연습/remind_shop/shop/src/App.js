0969ed8223df4e245f2a0dd07c4b1e0b1fa24292
2gyusung <97gyusung@gmail.com>
2022-11-18 07:59:50 +0900

    feat : React를 통해 쇼핑몰제작

---
+++ 프로젝트/연습/remind_shop/shop/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Navbar, Container, Nav} from 'react-bootstrap'
+
+
+function App() {
+  return (
+       <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
