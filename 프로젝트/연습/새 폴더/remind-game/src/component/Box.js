1ed8aaff24211f900d83d3d3eb530ed4b6880530
2gyusung <97gyusung@gmail.com>
2022-12-01 06:48:51 +0900

    fix : 수정

--- 프로젝트/연습/새 폴더/remind-game/src/component/Box.js
+++ 프로젝트/연습/새 폴더/remind-game/src/component/Box.js
@@ -3,15 +3,10 @@ import React from "react";
 const Box = (props) => {
   // console.log('props', props);
   let result;
-  if (
-    props.title === "Computer" &&
-    props.result !== "tie" &&
-    props.result !== ""
-  ) {
+  if (props.title === "Computer" && props.result !== "tie" &&  props.result !== "") {
     // 카드가 computer카드인가? && 결과가 비긴건 아닌가? && props.result에 값이 있는가?
     result = props.result === "win" ? "lose" : "win";
-  } else {
-    // 위의 경우가 아니라면 props&nbsp;로 전달된 결과를 그대로 쓴다.
+  } else {// 위의 경우가 아니라면 props&nbsp;로 전달된 결과를 그대로 쓴다.
     result = props.result;
   }
   return (
